a142409d8e1535aaff51a4bd44629868bf92467e
rajkumaryadav89 <rajkumar.yadav@ecolane.com>
2022-09-14 17:58:16 +0530

    Initial Commit

---
+++ CombineTutorial/CombineTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTutorialApp.swift
+//  CombineTutorial
+//
+//  Created by Rajkumar Yadav on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
